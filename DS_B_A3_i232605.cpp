e7863816c53977f6635be24ccbba38abd9040550
Malaika Zainab <malaikazainab@gmail.com>
2024-11-07 23:22:36 +0500

    making the Game class with id

--- DS_B_A3_i232605.cpp
+++ DS_B_A3_i232605.cpp
@@ -4,3 +4,31 @@
 #include <sstream>
 #include <cstdlib>
 #include <ctime>
+
+using namespace std;
+
+
+class GamePlayed 
+{
+public:
+string gameID;
+float hoursPlayed;
+int achievementUnlocked;
+GamePlayed* next;
+
+GamePlayed() : gameID(""), hoursPlayed(0.0), achievementUnlocked(0), next(nullptr) 
+{}
+};
+
+class Game 
+{
+public:
+string gameID;
+string name;
+string developer;
+string publisher;
+float fileSizeInGBs;
+int downloads;
+
+Game() : gameID(""), name(""), developer(""), publisher(""), fileSizeInGBs(0.0), downloads(0) {}
+};
